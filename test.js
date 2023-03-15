555700138cacccb624ea84c1977efbde622ea7df
Ikonde Nekemiah Arnold <i-nad@localhost.localdomain>
2023-03-15 08:24:38 +0300

    modified to have Hambuger menu popup window

--- test.js
+++ test.js
@@ -1,29 +1,12 @@
-// Get the elements
-const menu = document.querySelector('.menu');
-const links = document.querySelector('.links');
-const close = document.querySelector('.close');
+const menuButton = document.querySelector('.menu');
+const closeButton = document.querySelector('.close-popup');
+const menuPopup = document.querySelector('.menu-popup');
+const specificLinks = document.querySelectorAll('.specific-links a');
 
-// Add an event listener to the menu image
-menu.addEventListener('click', function() {
-   // Toggle the visibility of the mobile menu and close button
-   links.style.display = 'block';
-   close.style.display = 'block';
-   menu.style.display = 'none';
-});
+menuButton.addEventListener('click', toggleMenuPopup);
+closeButton.addEventListener('click', toggleMenuPopup);
+specificLinks.forEach(link => link.addEventListener('click', toggleMenuPopup));
 
-// Add an event listener to the close button
-close.addEventListener('click', function() {
-   // Toggle the visibility of the mobile menu and menu icon
-   links.style.display = 'none';
-   close.style.display = 'none';
-   menu.style.display = 'block';
-});
-
-// Add an event listener to the mobile links
-const mobileLinks = document.querySelectorAll('.mobile-link');
-mobileLinks.forEach(function(link) {
-  link.addEventListener('click', function() {
-    // Hide the mobile menu
-    links.style.display = 'none';
-  });
-});
+function toggleMenuPopup() {
+  menuPopup.classList.toggle('active');
+}
