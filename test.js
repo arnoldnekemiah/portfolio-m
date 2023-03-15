109f0375e6582f04f06236e3d5dae01dc46c9779
Ikonde Nekemiah Arnold <i-nad@localhost.localdomain>
2023-03-15 16:54:24 +0300

    Made changes as suggested by reviewer

--- test.js
+++ test.js
@@ -1,14 +1,20 @@
-
-
-document.getElementById('btn-menu').addEventListener('click', toggleMenuPopup)
-const ad = document.getElementById('links')
-const closeBtn = document.querySelector('.close-popup');
-
+const ad = document.getElementById('links');
 function toggleMenuPopup() {
   ad.classList.toggle('menu-popup');
 }
+document.getElementById('btn-menu').addEventListener('click', toggleMenuPopup);
+
+const closeBtn = document.querySelector('.close-popup');
 
 closeBtn.addEventListener('click', () => {
   ad.classList.remove('menu-popup');
-  document.body.classList.closeBtn('menu-open');
+  // document.body.classList.closeBtn('menu-open');
+});
+
+const links = document.querySelectorAll('.specific-links a');
+
+links.forEach((link) => {
+  link.addEventListener('click', () => {
+    ad.classList.remove('menu-popup');
+  });
 });
