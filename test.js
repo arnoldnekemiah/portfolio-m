5f6602fe1168dd2940e6708797a9e983b9a8ed96
Ikonde Nekemiah Arnold <i-nad@localhost.localdomain>
2023-03-15 18:26:35 +0300

    Made final changes to much the exact design of the figma template

--- test.js
+++ test.js
@@ -3,16 +3,11 @@ function toggleMenuPopup() {
   ad.classList.toggle('menu-popup');
 }
 document.getElementById('btn-menu').addEventListener('click', toggleMenuPopup);
-
 const closeBtn = document.querySelector('.close-popup');
-
 closeBtn.addEventListener('click', () => {
   ad.classList.remove('menu-popup');
-  // document.body.classList.closeBtn('menu-open');
 });
-
 const links = document.querySelectorAll('.specific-links a');
-
 links.forEach((link) => {
   link.addEventListener('click', () => {
     ad.classList.remove('menu-popup');
